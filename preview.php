68e99b6c49a0fb87e2683e58e4610113814b8e5e
Cinthia Sanchez Gallardo <chekis@urzuae.(none)>
2013-02-12 01:41:01 -0600

    Create first meme using font and background using merge function

--- preview.php
+++ preview.php
@@ -1,23 +1,29 @@
 <?php
  
-header("Content-type: image/png");
+header("Content-type: image/jpeg");
 
-$text = "Colibritany";
+$text = "Hola a todos nosotros";
 
-$imagen = imagecreatefromjpeg('pbzoosm.jpg');
- 
-$negro = imagecolorallocate($imagen,0,0,0);
+$fondo = imagecreatetruecolor(500, 400);
+$imagen = imagecreatefromjpeg('thumb-juke-asientos-piel-2.jpg');
+
+$negro = imagecolorallocate($imagen,255,255,255);
  
 imagesetpixel($imagen,30,30,$negro);
 
-$fuente = 'fonts/nissanag-regular-webfont.ttf';
+$fuente = './nissanag-bold-webfont.ttf';
 
 // Añadir algo de sombra al texto
-imagettftext($imagen, 20, 0, 11, 21, $negro, $fuente, $text);
+//imagettftext($imagen, 20, 0, 11, 21, $negro, $fuente, $text);
 
 // Añadir el texto
-imagettftext($imagen, 20, 0, 10, 20, $negro, $fuente, $text);
- 
-imagepng($imagen);
+imagettftext($fondo, 20, 0, 20, 350, $negro, $fuente, $text);
+
+imagefilledrectangle ( $imagen , 0 , 289 , 470 , 300 , $negro );
+
+imagecopymerge($fondo, $imagen, 15,10,0,0,470,289,100);
+
+imagejpeg($fondo);
  
 imagedestroy($imagen);
+imagedestroy($fondo);
